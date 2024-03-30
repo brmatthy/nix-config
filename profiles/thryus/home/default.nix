766390692a758a136ee42d3d88a1978398a4b688
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-03-30 21:45:26 +0100

    home default

---
+++ profiles/thryus/home/default.nix
@@ -0,0 +1,26 @@
+{ lib, config, options, pkgs, ... }:
+let
+  theme = import ../../../modules/home/themes/frappe.nix;
+in 
+{
+  imports = [
+    ../../../modules/home/hyprland.nix { # use hyprland as window manager
+      _module.args = {
+        inherit theme;
+      };
+    }
+
+    ../../../modules/home/look/gtk.nix { # set gtk theme
+      _module.args = {
+        inherit theme;
+      };
+    }
+
+    ../../../modules/home/look/cursor.nix { # set cursor theme
+      _module.args = {
+        inherit theme;
+      };
+    }
+  ];
+
+}
