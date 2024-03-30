efe45f16e2ac9bde709d194ac701e78d0734e965
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-03-30 21:58:49 +0100

    flake comments

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # home-manager.url = "github:nix-community/home-manager/release-23.11";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs,  ... }@inputs: {
     nixosConfigurations = {
       # Set configuration for each profile
       thryus = nixpkgs.lib.nixosSystem { # my main desktop
@@ -16,14 +16,14 @@
         modules = [
           ./profiles/thryus/system
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.extraSpecialArgs = inputs;
-            home-manager.users.brent = import ./profiles/thryus/home;
-          }
+          # home-manager.nixosModules.home-manager
+          # {
+          #   home-manager.useGlobalPkgs = true;
+          #   home-manager.useUserPackages = true;
+          #
+          #   home-manager.extraSpecialArgs = inputs;
+          #   home-manager.users.brent = import ./profiles/thryus/home;
+          # }
         ];
       };
     };
