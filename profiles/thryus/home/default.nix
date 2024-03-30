64558140eb5d0181d9eca5afc89a697eb4248f42
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-03-30 22:00:10 +0100

    uncomment

--- profiles/thryus/home/default.nix
+++ profiles/thryus/home/default.nix
@@ -4,24 +4,24 @@ let
 in 
 {
   home.stateVersion = "23.11";
-  # imports = [
-  #   ../../../modules/home/hyprland.nix { # use hyprland as window manager
-  #     _module.args = {
-  #       inherit theme;
-  #     };
-  #   }
-  #
-  #   ../../../modules/home/look/gtk.nix { # set gtk theme
-  #     _module.args = {
-  #       inherit theme;
-  #     };
-  #   }
-  #
-  #   ../../../modules/home/look/cursor.nix { # set cursor theme
-  #     _module.args = {
-  #       inherit theme;
-  #     };
-  #   }
-  # ];
+  imports = [
+    ../../../modules/home/hyprland.nix { # use hyprland as window manager
+      _module.args = {
+        inherit theme;
+      };
+    }
+
+    ../../../modules/home/look/gtk.nix { # set gtk theme
+      _module.args = {
+        inherit theme;
+      };
+    }
+
+    ../../../modules/home/look/cursor.nix { # set cursor theme
+      _module.args = {
+        inherit theme;
+      };
+    }
+  ];
 
 }
