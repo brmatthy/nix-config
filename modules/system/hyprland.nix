ff39e84a820a1b549c27b29e840037259ff89491
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-04-16 20:24:23 +0200

    hypr simple

--- modules/system/hyprland.nix
+++ modules/system/hyprland.nix
@@ -1,25 +1,25 @@
 { lib, config, options, pkgs, ... }:
 {
   # Security
-  security = {
-    pam.services.swaylock = {
-      text = ''
-        auth include login
-      '';
-    };
-#    pam.services.gtklock = {};
-    pam.services.login.enableGnomeKeyring = true;
-  };
-
-  services.gnome.gnome-keyring.enable = true;
+#   security = {
+#     pam.services.swaylock = {
+#       text = ''
+#         auth include login
+#       '';
+#     };
+# #    pam.services.gtklock = {};
+#     pam.services.login.enableGnomeKeyring = true;
+#   };
+#
+#   services.gnome.gnome-keyring.enable = true;
 
   programs = {
     hyprland = {
       enable = true;
-      xwayland = {
-        enable = true;
-      };
-      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+      # xwayland = {
+      #   enable = true;
+      # };
+      # portalPackage = pkgs.xdg-desktop-portal-hyprland;
     };
   };
 }
