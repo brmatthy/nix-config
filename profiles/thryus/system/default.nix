2f6fffdfdb78e1593269a7aaa8e73d0ac2a8370a
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-04-16 19:41:13 +0200

    hyprland sys

--- profiles/thryus/system/default.nix
+++ profiles/thryus/system/default.nix
@@ -40,7 +40,7 @@ in
     # ../../../modules/system/fonts.nix # install fonts
 
     ../../../modules/system/wayland-sddm.nix # use sddm as wayland display manager
-    # ../../../modules/system/hyprland.nix # use hyprland as the window manager
+    ../../../modules/system/hyprland.nix # use hyprland as the window manager
     # ../../../modules/system/dbus.nix
     # ../../../modules/system/pulseaudio.nix # use pulseaudio for sound
     # ../../../modules/system/polkit.nix # start polkit on startup
