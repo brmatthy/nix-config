028e7eb739beca703c1b4bee2da6fc296c957d4b
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-06-03 20:46:01 +0200

    update cursors

--- stylix/catppuccin-frappe/default.nix
+++ stylix/catppuccin-frappe/default.nix
@@ -3,8 +3,8 @@
   stylix.image = ./nixos-dark.jpg;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
 
-  stylix.cursor.package = pkgs.catppuccin-cursors;
-  stylix.cursor.name = "frappePeach";
+  stylix.cursor.package = pkgs.catppuccin-cursors.frappePeach;
+  #stylix.cursor.name = "";
   stylix.cursor.size = 24;
 
 }
