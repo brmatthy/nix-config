5d7565f69a36963894a32de0845b2aab16c31842
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-06-03 20:54:52 +0200

    cursors

--- stylix/catppuccin-frappe/default.nix
+++ stylix/catppuccin-frappe/default.nix
@@ -3,8 +3,8 @@
   stylix.image = ./nixos-dark.jpg;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
 
-  stylix.cursor.package = pkgs.catppuccin-cursors.frappePeach;
-  #stylix.cursor.name = "";
+  stylix.cursor.package = pkgs.bibata-cursors;
+  stylix.cursor.name = "Bibata-Modern-Classic";
   stylix.cursor.size = 24;
 
 }
--- stylix/catppuccin-latte/default.nix
+++ stylix/catppuccin-latte/default.nix
@@ -3,8 +3,8 @@
   stylix.image = ./nixos-light.jpg;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-latte.yaml";
 
-  stylix.cursor.package = pkgs.catppuccin-cursors;
-  stylix.cursor.name = "lattePeach";
+  stylix.cursor.package = pkgs.bibata-cursors;
+  stylix.cursor.name = "Bibata-Modern-Classic";
   stylix.cursor.size = 24;
 
 }
