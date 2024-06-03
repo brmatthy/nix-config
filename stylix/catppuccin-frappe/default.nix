ef683b8931ee7ed13da51df0dfec19a4619471e9
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-06-03 20:43:04 +0200

    stylix cursor theme

--- stylix/catppuccin-frappe/default.nix
+++ stylix/catppuccin-frappe/default.nix
@@ -3,4 +3,8 @@
   stylix.image = ./nixos-dark.jpg;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
 
+  stylix.cursor.package = pkgs.catppuccin-cursors;
+  stylix.cursor.name = "frappePeach";
+  stylix.cursor.size = 24;
+
 }
--- stylix/catppuccin-latte/default.nix
+++ stylix/catppuccin-latte/default.nix
@@ -2,4 +2,9 @@
 {
   stylix.image = ./nixos-light.jpg;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-latte.yaml";
+
+  stylix.cursor.package = pkgs.catppuccin-cursors;
+  stylix.cursor.name = "lattePeach";
+  stylix.cursor.size = 24;
+
 }
