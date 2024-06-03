54ff1382ba0532c238472c483ee4fca87d7d62ac
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-06-03 19:09:23 +0200

    zsh

--- home/terminal/zsh.nix
+++ home/terminal/zsh.nix
@@ -2,7 +2,7 @@
 {
   programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
+    autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
     enableCompletion = true;
     shellAliases = aliases;
