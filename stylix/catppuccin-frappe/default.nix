ffce4206fc70551cb09126eaf12327e6605b115e
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-06-03 21:42:44 +0200

    move font to user

--- stylix/catppuccin-frappe/default.nix
+++ stylix/catppuccin-frappe/default.nix
@@ -6,27 +6,4 @@
   stylix.cursor.package = pkgs.bibata-cursors;
   stylix.cursor.name = "Bibata-Original-Amber";
   stylix.cursor.size = 20;
-
-  stylix.fonts = {
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-
-    monospace = {
-      package = pkgs.nerdfonts.override {fonts = ["RobotoMono"];};
-      name = "RobotoMono Nerd Font";
-    };
-
-    emoji = {
-      package = pkgs.noto-fonts-emoji;
-      name = "Noto Color Emoji";
-    };
-  };
-
 }
--- users/brent.nix
+++ users/brent.nix
@@ -30,6 +30,30 @@ in
 
   stylix.targets.vim.enable = false;
 
+  # set fonts
+  stylix.fonts = {
+    serif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Serif";
+    };
+
+    sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+    };
+
+    monospace = {
+      package = pkgs.nerdfonts.override {fonts = ["RobotoMono"];};
+      name = "RobotoMono Nerd Font";
+    };
+
+    emoji = {
+      package = pkgs.noto-fonts-emoji;
+      name = "Noto Color Emoji";
+    };
+  };
+
+
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     # dev
