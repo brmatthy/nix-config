ec02ab645a40858386ff0be618cd7b1ea6bbb867
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-06-03 21:40:19 +0200

    set fonts

--- stylix/catppuccin-frappe/default.nix
+++ stylix/catppuccin-frappe/default.nix
@@ -7,4 +7,26 @@
   stylix.cursor.name = "Bibata-Original-Amber";
   stylix.cursor.size = 20;
 
+  stylix.fonts = {
+    serif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Serif";
+    };
+
+    sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+    };
+
+    monospace = {
+      package = pkgs.nerdfonts.override {fonts = ["RobotoMono"];};
+      name = "RobotoMono Nerd Font";
+    };
+
+    emoji = {
+      package = pkgs.noto-fonts-emoji;
+      name = "Noto Color Emoji";
+    };
+  };
+
 }
