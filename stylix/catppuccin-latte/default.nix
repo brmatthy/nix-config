5c7e681619be0b788d3b02c506440a9507c04f76
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-06-03 20:58:32 +0200

    cursor smaller

--- stylix/catppuccin-latte/default.nix
+++ stylix/catppuccin-latte/default.nix
@@ -4,7 +4,7 @@
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-latte.yaml";
 
   stylix.cursor.package = pkgs.bibata-cursors;
-  stylix.cursor.name = "Bibata-Modern-Classic";
-  stylix.cursor.size = 24;
+  stylix.cursor.name = "Bibata-Modern-Amber-Right";
+  stylix.cursor.size = 20;
 
 }
