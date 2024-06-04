124439dd2c17ba241784de5c9bf0886025178c52
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-06-04 13:24:49 +0200

    plymouth

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}:
 {
+  boot.kernelParams = ["quiet"];
   boot.plymouth.enable = true; # nice splash screen on boot
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
