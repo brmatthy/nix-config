b26f5024bfb10068a4e563aa0b7fb86334bef2c0
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-06-06 09:22:39 +0200

    stylix set default color

--- stylix/catppuccin-frappe/default.nix
+++ stylix/catppuccin-frappe/default.nix
@@ -2,6 +2,10 @@
 {
   stylix.image = ./nixos-dark.jpg;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
+  stylix.override = {
+    base09 = "e5c890";
+    base0A = "ef9f76";
+  };
 
   stylix.cursor.package = pkgs.bibata-cursors;
   stylix.cursor.name = "Bibata-Original-Amber";
