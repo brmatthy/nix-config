b8e920c214dddc3653cd1b1c0795e93033284669
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-06-10 19:33:06 +0200

    waybar: brightness stylin

--- home/desktop/waybar/modules/brightness.nix
+++ home/desktop/waybar/modules/brightness.nix
@@ -1,4 +1,4 @@
-{...}:
+{lib, ...}:
 {
   programs.waybar.settings."mainbar"."group/brightness" = {
     orientation = "inherit";
@@ -41,4 +41,31 @@
     max = 100;
     orientation = "vertical";
   };
+
+
+  programs.waybar.style = lib.mkAfter
+  ''
+    #backlight {
+      color: @text;
+    }
+
+    #backlight-slider slider {
+      background-color: transparent;
+      box-shadow: none;
+    }
+
+    #backlight-slider highlight{
+      border-radius: 8px;
+      background-color: @text;
+    }
+
+    #backlight-slider trough {
+      margin-top: 4px;
+      min-width: 6px;
+      min-height: 60px;
+      border-radius: 8px;
+      border-style: solid;
+      border-color: @text;
+    }
+  '';
 }
