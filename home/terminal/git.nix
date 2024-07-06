f2e585beb2f362a88df4706e31f807d1a6c82fb2
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-07-06 20:35:49 +0200

    gitconfig

--- home/terminal/git.nix
+++ home/terminal/git.nix
@@ -4,5 +4,13 @@
     enable = true;
     userName = "brmatthy";
     userEmail = "109791839+brmatthy@users.noreply.github.com";
+    extraConfig = {
+      push = {
+        autoSetupRemote = true;
+      };
+      pull = {
+        rebase = false;
+      };
+    };
   };
 }
