9ebc94c23537a3432b5702af6744eacc13d401bf
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-07-06 21:50:11 +0200

    kanshi

--- home/desktop/kanshi.nix
+++ home/desktop/kanshi.nix
@@ -15,7 +15,7 @@
         ];
       };
 
-      docked_home = {
+      zyphron_docked_home = {
         outputs = [
           {
             criteria = "Iiyama North America PLX2283H 11593014B8288";
@@ -33,6 +33,24 @@
           {
             criteria = "eDP-1";
             status = "disable";
+          }
+        ];
+      };
+
+      thryus_docked_home = {
+        outputs = [
+          {
+            criteria = "Iiyama North America PLX2283H 11593014B8288";
+            position = "1920,-560";
+            transform = "90";
+            scale = 1.0;
+            mode = "1920x1080@60Hz";
+          }
+          {
+            criteria = "LG Electronics LG FULL HD 812NTWGCU585";
+            position = "0,0";
+            scale = 1.0;
+            mode = "1920x1080@60Hz";
           }
         ];
       };
--- users/brent.nix
+++ users/brent.nix
@@ -12,8 +12,6 @@ let
     docked="wlr-randr --output eDP-1 --off --output DP-1 --pos 0,0 --transform normal --output HDMI-A-1 --pos 1920,-560 --transform 90";
   };
 
-  kanshi_path = "../home/desktop/kanshi/${hostname}.nix";
-
   sharedImports = [
     ../home/fonts.nix
     ../home/terminal/git.nix
@@ -22,7 +20,7 @@ let
     ../home/terminal/starship.nix
     ../home/terminal/zsh.nix
     { _module.args = { inherit aliases; }; }
-    (import kanshi_path)
+    ../home/desktop/kanshi.nix
   ];
 
   machineImports = [
