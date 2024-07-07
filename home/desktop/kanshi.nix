791f9910fa19411fae947b80e407f0cfddaf384b
brmatthy <109791839+brmatthy@users.noreply.github.com>
2024-07-07 10:19:50 +0200

    kanshi

--- home/desktop/kanshi.nix
+++ home/desktop/kanshi.nix
@@ -47,7 +47,7 @@
             mode = "1920x1080@60Hz";
           }
           {
-            criteria = "LG Electronics LG FULL HD 812NTWGCU585";
+            criteria = "LG Electronics LG FULL HD";
             position = "0,0";
             scale = 1.0;
             mode = "1920x1080@60Hz";
